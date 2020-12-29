ef1628e468e8e3f7a374d72e2f13975a9f8c6e4b
Sergey Moskvin <debug45@mail.ru>
2020-12-29 13:38:02 +0500

    Создан пустой проект

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sergey Moskvin on 29.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
