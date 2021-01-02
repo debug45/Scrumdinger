fc4b9eb64a5e1573523414d645e84e76d6386d64
Sergey Moskvin <debug45@mail.ru>
2021-01-02 12:20:04 +0500

    Завершена секция «Transcribing Speech to Text»

--- Scrumdinger/MeetingTimerView.swift
+++ Scrumdinger/MeetingTimerView.swift
@@ -32,6 +32,7 @@ struct SpeakerArc: Shape {
 
 struct MeetingTimerView: View {
     @Binding var speakers: [ScrumTimer.Speaker]
+    @Binding var isRecording: Bool
     var scrumColor: Color
     private var currentSpeaker: String { speakers.first(where: { !$0.isCompleted })?.name ?? "Someone" }
     var body: some View {
@@ -42,6 +43,10 @@ struct MeetingTimerView: View {
                 Text(currentSpeaker)
                     .font(.title)
                 Text("is speaking")
+                Image(systemName: isRecording ? "mic" : "mic.slash")
+                    .font(.title)
+                    .padding(.top)
+                    .accessibilityLabel(isRecording ? "with transcription" : "without transcription")
             }
             .accessibilityElement(children: .combine)
             .foregroundColor(scrumColor.accessibleFontColor)
@@ -60,6 +65,6 @@ struct MeetingTimerView: View {
 struct MeetingTimerView_Previews: PreviewProvider {
     @State static var speakers = [ScrumTimer.Speaker(name: "Kim", isCompleted: true), ScrumTimer.Speaker(name: "Bill", isCompleted: false)]
     static var previews: some View {
-        MeetingTimerView(speakers: $speakers, scrumColor: Color("Design"))
+        MeetingTimerView(speakers: $speakers, isRecording: .constant(true), scrumColor: Color("Design"))
     }
 }
